cdba359d966b24d8cba7af03a1b898eb721b8e57
Elodie Ferrais <elodie.ferrais@gmail.com>
2014-03-13 15:22:55 -0700

    save cryptotype in shared preferences

--- api/src/main/java/com/eferrais/api/helper/SharedPreferencesCryptoHelper.java
+++ api/src/main/java/com/eferrais/api/helper/SharedPreferencesCryptoHelper.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
-
 import com.eferrais.api.converstionrate.CryptoType;
 
 import java.io.IOException;
@@ -15,7 +14,7 @@ import java.io.IOException;
 public class SharedPreferencesCryptoHelper {
     private static SharedPreferences sharedPreferences;
     private String MY_PREFERENCES = "com.eferrais.api:PREFERENCES";
-    private String MY_PREFERENCES_CRYPTO = "com.eferrais.api:PREFERENCES:CRYPTO";
+    private String MY_PREFERENCES_CRYPTO_PREFIX = "com.eferrais.api:PREFERENCES:CRYPTO";
 
     public SharedPreferencesCryptoHelper(Context context) {
         sharedPreferences = context.getSharedPreferences(MY_PREFERENCES, context.MODE_PRIVATE);
@@ -23,30 +22,29 @@ public class SharedPreferencesCryptoHelper {
 
     public void saveCrypto(CryptoType cryptoType) {
         try {
-            sharedPreferences.edit().putString(MY_PREFERENCES_CRYPTO, SerializableHelper.toString(cryptoType)).commit();
+            sharedPreferences.edit().putString(getPreferencesCryptoKey(cryptoType), SerializableHelper.toString(cryptoType)).commit();
         } catch (IOException e) {
             Log.e(SharedPreferencesCryptoHelper.class.getName(), "The following crypto have not been saved: " + cryptoType.getLabel(), e);
         }
     }
 
     public void updateCryptos() {
-            String cryptoSerialized = sharedPreferences.getString(MY_PREFERENCES_CRYPTO, null);
+        CryptoType[] cryptoTypeValues = CryptoType.values();
+        for (int i = 0; i < cryptoTypeValues.length; i++) {
+            String cryptoSerialized = sharedPreferences.getString(getPreferencesCryptoKey(cryptoTypeValues[i]), null);
             try {
                 if (cryptoSerialized != null) {
                     SerializableHelper.fromString(cryptoSerialized);
                 }
-                /**for(CryptoType crypo:CryptoType.values()) {
-                    crypo.setChangeRateToBTC(cryptoType.getChangeRateToBTC());
-                }
-
-                CryptoType.BITCOIN.setChangeRateToBTC(cryptoType.getChangeRateToBTC());**/
-
             } catch (IOException e) {
                 Log.e(SharedPreferencesCryptoHelper.class.getName(), "The following crypto type have not been de-serialized: " + cryptoSerialized, e);
             } catch (ClassNotFoundException e) {
                 Log.e(SharedPreferencesCryptoHelper.class.getName(), "The following crypto type have not been de-serialized: " + cryptoSerialized, e);
-
             }
+        }
+    }
 
+    private String getPreferencesCryptoKey(CryptoType cryptoType) {
+        return MY_PREFERENCES_CRYPTO_PREFIX + "_" + cryptoType.getLabel().toUpperCase();
     }
 }
